8723c186d3100d98be84c02b1f558075d7f125b6
JobbyM <songzhipengmune@gmail.com>
2016-04-26 23:32:01 +0800

    chapter5.js create sub file

--- chrome_app_filesystem_api/chapter5.js
+++ chrome_app_filesystem_api/chapter5.js
@@ -1,7 +1,10 @@
 document.getElementById('createFile').addEventListener('click',function(){
   chrome.fileSystem.chooseEntry({type: 'openDirectory'},function(Entry){
-    Entry.getDirectory('new_folder',{create: true},function(subEntry){
-      // We'll do something with subEntry later
+    // Entry.getDirectory('new_folder',{create: true},function(subEntry){
+    //   // We'll do something with subEntry later
+    // },errorHandler);
+    Entry.getFile('log.txt', {create: true}, function(fileEntry){
+      // We'll do someting with fileEntry
     },errorHandler);
   });
 },false);
