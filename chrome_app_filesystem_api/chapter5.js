bed064b050ff3ee7a5e13c2fe0613c8b39d44a51
JobbyM <songzhipengmune@gmail.com>
2016-04-26 23:41:42 +0800

    add another create file method in chapter5.js

--- chrome_app_filesystem_api/chapter5.js
+++ chrome_app_filesystem_api/chapter5.js
@@ -1,10 +1,16 @@
 document.getElementById('createFile').addEventListener('click',function(){
-  chrome.fileSystem.chooseEntry({type: 'openDirectory'},function(Entry){
-    // Entry.getDirectory('new_folder',{create: true},function(subEntry){
-    //   // We'll do something with subEntry later
-    // },errorHandler);
-    Entry.getFile('log.txt', {create: true}, function(fileEntry){
-      // We'll do someting with fileEntry
-    },errorHandler);
+  // chrome.fileSystem.chooseEntry({type: 'openDirectory'},function(Entry){
+  //   // Entry.getDirectory('new_folder',{create: true},function(subEntry){
+  //   //   // We'll do something with subEntry later
+  //   // },errorHandler);
+  //   Entry.getFile('log.txt', {create: true}, function(fileEntry){
+  //     // We'll do someting with fileEntry
+  //   },errorHandler);
+  // });
+  chrome.fileSystem.chooseEntry({
+    type: 'saveFile',
+    suggestedName: 'log.txt'
+  }, function(fileEntry){
+    // We'll do something with fileEntry later
   });
 },false);
