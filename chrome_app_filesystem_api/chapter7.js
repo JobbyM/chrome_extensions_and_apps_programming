ffc59b22ef56e333ea79db95d8e07023ea28438d
JobbyM <songzhipengmune@gmail.com>
2016-04-27 15:31:58 +0800

    add fileWriter method in chapter7.js

--- chrome_app_filesystem_api/chapter7.js
+++ chrome_app_filesystem_api/chapter7.js
@@ -1,17 +1,27 @@
 document.getElementById('fileWriter').addEventListener('click',function(){
-  chrome.fileSystem.chooseEntry({type: 'openDirectory'},function(Entry){
-    Entry.getFile('log.txt',{},function(fileEntry){
-      fileEntry.createWriter(function(fileWriter){
-        // We'll do something with fileWriter later
-      },errorHandler);
-    },errorHandler);
-  });
-  // chrome.fileSystem.chooseEntry({
-  //   type: 'saveFile',
-  //   suggestedName: 'log.txt'
-  // },function(fileEntry){
-  //   fileEntry.createWriter(function(fileWriter){
-  //     // We'll do something with fileWriter later
+  // chrome.fileSystem.chooseEntry({type: 'openDirectory'},function(Entry){
+  //   Entry.getFile('log.txt',{},function(fileEntry){
+  //     fileEntry.createWriter(function(fileWriter){
+  //       // We'll do something with fileWriter later
+  //
+  //     },errorHandler);
   //   },errorHandler);
   // });
+  chrome.fileSystem.chooseEntry({
+    type: 'saveFile',
+    suggestedName: 'log.txt'
+  },function(fileEntry){
+    fileEntry.createWriter(function(fileWriter){
+      // We'll do something with fileWriter later
+      fileWriter.write(new Blob(['Hello World'], {type: 'text/plain'}));
+
+      fileWriter.onwrite = function(){
+        console.log('Write begin.');
+      }
+
+      fileWriter.onwriteend = function(){
+        console.log('Write end.');
+      }
+    },errorHandler);
+  });
 },false);
