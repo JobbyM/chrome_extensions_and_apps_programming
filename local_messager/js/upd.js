1e1f429bd2dd9d035c177bf17342f3a41906ccaa
JobbyM <songzhipengmune@gmail.com>
2016-04-30 21:38:54 +0800

    add upd.js send & onReceive & onReceiveError

--- local_messager/js/upd.js
+++ local_messager/js/upd.js
@@ -28,9 +28,38 @@ function dup(){
     _upd.close(this.socketId, callback);
   }.bind(this),
 
+  this.send = function(address, port, data, callback){
+    _upd.send(this.socketId, data, address, port, callback);
+  }.bind(this),
+
+  this.receive = function(info){
+    console.log('Receive data from ' + info.remoteAddress + ':' + info.remotePort);
+  },
+
+  this.error = function(code){
+    console.log('An erro occured with code ' + code);
+  },
+
   this.init = function(callback){
     this.create(function(){
-      this.bind(callback);
+      this.bind(function(code){
+          if(code<0){
+            this.error(code);
+            return false;
+          }else{
+            callback();
+          }
+      }.bind(this));
+      _upd.onReceive.addListener(function(info){
+        if(info.socketId = this.socketId){
+          this.receive(info);
+        }
+      }.bind(this));
+      _upd.onReceiveError.addListener(function(info){
+        if(info.socketId == this.socketId){
+          this.error(info.resultCode);
+        }
+      }.bind(this);
     }.bind(this));
   }.bind(this)
 }
