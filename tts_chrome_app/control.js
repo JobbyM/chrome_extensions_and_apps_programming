a03397cb0400bab237a128cd59f7bd2510fafe32
JobbyM <songzhipengmune@gmail.com>
2016-05-03 15:25:07 +0800

    add chrome.tts. speak stop pause resume in tts_chrome_app

--- tts_chrome_app/control.js
+++ tts_chrome_app/control.js
@@ -13,3 +13,25 @@ document.getElementById('maximize').addEventListener('click',function(){
     current_window.restore():
     current_window.maximize();
 },false);
+
+document.getElementById('speak').addEventListener('click',function(){
+  chrome.tts.speak('Hello, world.');
+  chrome.tts.speak('Speak this next, when the first sentence is done.',{enqueue: true});
+  // chrome.tts.speak(utterance, options, function(){
+  //   if(chrome.runtime.lastError){
+  //     console.log('Error: ' + chrome.runtime.lastError.message);
+  //   }
+  // });
+},false);
+
+document.getElementById('stop').addEventListener('click',function(){
+  chrome.tts.stop();
+},false);
+
+document.getElementById('pause').addEventListener('click',function(){
+  chrome.tts.pause();
+},false);
+
+document.getElementById('resume').addEventListener('click',function(){
+  chrome.tts.resume();
+},false);
