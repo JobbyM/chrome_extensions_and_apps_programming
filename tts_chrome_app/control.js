cc22aca71ae4db2494399ad4dabec10e31664ec2
JobbyM <songzhipengmune@gmail.com>
2016-05-04 10:27:35 +0800

    add sing a song in tts_chrome_app

--- tts_chrome_app/control.js
+++ tts_chrome_app/control.js
@@ -64,12 +64,104 @@ document.getElementById('getVoices').addEventListener('click',function(){
       chrome.tts.speak(utterance,{
         voiceName: voices[i].voiceName,
         lang: voices[i].lang,
-        enqueue: true
+        enqueue: true,
+        onEvent: function(event){
+          console.log('Event ' + event.type + ' at position' + event.charIndex);
+          if(event.type == 'error'){
+            console.log('Error: ' + event.errorMessage);
+          }
+        }
       },function(){
         if(chrome.runtime.lastError){
           console.log('Error: ' + chrome.runtime.lastError.message);
         }
-      })
+      });
+    }
+  });
+},false);
+
+document.getElementById('song').addEventListener('click',function(){
+  var starsky = ['Star Sky - Two Steps From Hell',
+  'Here we are',
+  'Riding the sky',
+  'Painting the night with sun',
+  'You and I Mirrors of light',
+  'Twin flames of fire',
+  'Lit in another time and place',
+  'I knew your name',
+  'I knew your face',
+  'Your love and grace',
+  'Past and present now embrace',
+  'Worlds collide in inner space',
+  'Unstoppable the song we play',
+  'Burn the page for me',
+  'I cannot erase the time of sleep',
+  'I cannot be loved so set me free',
+  'I cannot deliver your love',
+  'Or caress your soul so',
+  'turn that page for me',
+  'I cannot embrace the touch that you give',
+  'I cannot find solace in your words',
+  'I cannot deliver you your love',
+  'or caress your soul',
+  'Age to age',
+  'I feel the call',
+  'Memory of future dreams',
+  'You and I riding the sky',
+  'Keeping the fire bright',
+  'From another time and place',
+  'I know your name',
+  'I know your face',
+  'Your touch and grace',
+  'All of time can not erase',
+  'What our hearts remember stays',
+  'Forever on a song we play',
+  'Burn the page for me',
+  'I cannot erase the time of sleep',
+  'I cannot be loved so set me free',
+  'I cannot deliver your love',
+  'Or caress your soul so',
+  'turn that page for me',
+  'I cannot embrace the touch that you give',
+  'I cannot find solace in your words',
+  'I cannot deliver you your love',
+  'or caress your soul'];
+  console.log(starsky);
+  chrome.tts.getVoices(function(voices){
+    console.log(voices);
+    var vl = voices.length;
+    var random = 0;
+    var utterance = '';
+    var voiceName = 'Google 日本語';
+    var lang = 'ja-JP';
+    for(var i = 0, len = starsky.length; i < len; i ++){
+
+      random = Math.floor(Math.random()*vl);
+      if(!random){
+        random = 1;
+      }
+      utterance = starsky[i];
+      voiceName = voices[random].voiceName;
+      lang = voices[random].lang;
+      console.log('utterance:' + utterance);
+      console.log('random:' + random);
+      console.log('voiceName:' + voiceName);
+      console.log('lang:' + lang);
+      chrome.tts.speak(utterance,{
+        'voiceName': voiceName,
+        'lang': lang,
+        'enqueue': true,
+        'onEvent': function(event){
+          console.log('Event ' + event.type + ' at position' + event.charIndex);
+          if(event.type == 'error'){
+            console.log('Error: ' + event.errorMessage);
+          }
+        }
+      },function(){
+        if(chrome.runtime.lastError){
+          console.log('Error: ' + chrome.runtime.lastError.message);
+        }
+      });
     }
   });
 },false);
