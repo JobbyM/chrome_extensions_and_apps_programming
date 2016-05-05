7e4398b4b9df89c9f8799dbe9ce4ac2e58ae3160
JobbyM <songzhipengmune@gmail.com>
2016-05-05 13:06:05 +0800

    initial eden_music

---
+++ eden_music/background.js
@@ -0,0 +1,35 @@
+chrome.app.runtime.onLaunched.addListener(function(){
+  var main_window = chrome.app.window.get('main');
+  if(main_window){
+    main_window.show();
+  }else{
+    chrome.app.window.create('main.html',{
+      'id': 'main',
+      'frame':'none'
+    });
+  }
+});
+
+chrome.app.window.onFullscreened.addListener(function(){
+  // do something when the window is set to fullsreeen;
+});
+
+chrome.app.window.onMaximized.addListener(function(){
+  // do something when the window is set to maximized;
+});
+
+chrome.app.window.onMinimized.addListener(function(){
+  // do something when the window is set to minimized;
+});
+
+chrome.app.window.onRestored.addListener(function(){
+  // do something when the window is set to restored;
+});
+
+chrome.app.window.onBoundsChanged.addListener(function(){
+  // do something when the window is set to resized;
+});
+
+chrome.app.window.onClosed.addListener(function(){
+  // do something when the window is set to closed;
+});
